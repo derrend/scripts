280158192df7386c1ed3562e5b01dc876f9b0f9a
Derren Desouza <derrend@yahoo.co.uk>
2015-03-16 10:42:25 +1000

    Update tables.sh

--- bash/tables.sh
+++ bash/tables.sh
@@ -26,19 +26,25 @@ function udplist {
     fi
 }
 
-# Define initial paramiters
+# Set initial conditions
 $IPT -F
+$IPT -t nat -F
 
 $IPT -P INPUT DROP
 $IPT -P FORWARD DROP
 $IPT -P OUTPUT ACCEPT
 
-# Define dynamic parameters
+# Define primary parameters
 tcplist $(cat $TCPLIST)
 udplist $(cat $UDPLIST)
 
 # Define secondary parameters
 $IPT -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Save current configuration
+# Host as router
+#$IPT -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#$IPT -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#$IPT -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
+# Save configuration
 #service iptables save
