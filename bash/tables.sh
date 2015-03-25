06b5a2117b21c24762df0eb6854c1bf35ab6a787
Derren Desouza <derrend@yahoo.co.uk>
2015-03-25 13:29:47 +1000

    Update tables.sh

--- bash/tables.sh
+++ bash/tables.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -x
 
+# **DEFINE CUSTOM RULES NEAR END OF SCRIPT**
+
+# Set variables
 IPT=$(which iptables)
 
 TCPLIST=/tmp/TCPLIST
 UDPLIST=/tmp/UDPLIST
 
-# Capture listening ports
+# Listening ports
 ss -lnt | awk '{print $4;}' | rev | cut -f 1 -d : | rev | sort | uniq | head -n -1 > $TCPLIST
 ss -lnu | awk '{print $4;}' | rev | cut -f 1 -d : | rev | sort | uniq | head -n -1 > $UDPLIST
 
-# Functions dynamically allow access on listening ports
+# Open ports
 function tcplist {
     if [ $# -ne 0 ]; then
         for i in $@; do
@@ -26,25 +29,34 @@ function udplist {
     fi
 }
 
-# Set initial conditions
+# Initial paramiters
 $IPT -F
 $IPT -t nat -F
+
+$IPT -N EXPLICIT
 
 $IPT -P INPUT DROP
 $IPT -P FORWARD DROP
 $IPT -P OUTPUT ACCEPT
 
-# Define primary parameters
+$IPT -I INPUT -j EXPLICIT
+
+# Primary paramiters **SEE CUSTOM RULES NEAR END OF SCRIPT**
+
+# Secondary parameters
 tcplist $(cat $TCPLIST) #<-- Port numbers may be manually appended here
 udplist $(cat $UDPLIST) #<-- Port numbers may be manually appended here
 
-# Define secondary parameters
+# Closing parameters
 $IPT -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Host as router
+# Router settings
 #$IPT -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #$IPT -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 #$IPT -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
+# Primary paramiters **DEFINE CUSTOM RULES HERE**
+#$IPT -A EXPLICIT -p tcp --syn --dport 22 -m connlimit --connlimit-above 2 -j REJECT
 
 # Save configuration
 #service iptables save
