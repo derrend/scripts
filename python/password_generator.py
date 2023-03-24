c95bb7d7d8aed4abfb17e8747945bea92ff66f42
derrend <derrend@yahoo.co.uk>
2023-03-24 18:55:38 +1000

    Update password_generator.py

--- python/password_generator.py
+++ python/password_generator.py
@@ -6,22 +6,39 @@ import random
 import string
 from sys import argv
 
+def generate_password(seed_phrase, password_length):
+    random.seed(seed_phrase)
+    password = ''
+    for x in range(password_length):
+        password += random.choice(string.ascii_letters + string.digits)
+
+    return password
+
+## Uncomment to enable test mode.
+# test_seed = 'test16749442'
+
+if len(argv) == 2:  ## Test mode.
+
+    try:
+        print(generate_password(test_seed, int(argv[1])))
+        exit(0)
+
+    except ValueError:
+        print('Password length must be an intiger.')
+        exit(1)
+
+    except NameError:
+        print('Test mode disabled.')
+        exit(1)
+
 if len(argv) != 3:
     print('Please supply a password length and seed.')
     exit(1)
 
 try:
-    password_length = int(argv[1])
+    print(generate_password(argv[2], int(argv[1])))
+    exit(0)
 
 except ValueError:
     print('Password length must be an intiger.')
     exit(1)
-
-seed_phrase = argv[2]
-password = ''
-
-random.seed(seed_phrase)
-for x in range(password_length):
-    password += random.choice(string.ascii_letters + string.digits)
-
-print(password)
