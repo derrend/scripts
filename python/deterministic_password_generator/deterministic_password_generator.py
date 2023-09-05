74cbe64d917d765a16a375b211be8e327e363d52
derrend <derrend@yahoo.co.uk>
2023-09-05 12:39:45 +1000

    Add files via upload

---
+++ python/deterministic_password_generator/deterministic_password_generator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+##Python 3.10.6
+
+## Generate deterministic password from seed phrase.
+import random
+import string
+from sys import argv
+
+def generate_password(seed_phrase, password_length):
+
+    random.seed(seed_phrase)
+    password = ''
+
+    for x in range(password_length):
+        password += random.choice(string.ascii_letters + string.digits)
+
+    return password
+
+## Uncomment to enable test mode.
+# test_seed = 'test16749442'
+
+if len(argv) == 2:  ## Test mode.
+
+    try:
+        print(generate_password(test_seed, int(argv[1])))
+        exit(0)
+
+    except ValueError:
+        print('Password length must be an integer.')
+        exit(1)
+
+    except NameError:
+        print('Test mode disabled.')
+        exit(1)
+
+if len(argv) != 3:
+    print('Please supply a password length and seed.')
+    exit(1)
+
+try:
+    print(generate_password(argv[2], int(argv[1])))
+    exit(0)
+
+except ValueError:
+    print('Password length must be an integer.')
+    exit(1)
+
